990e159e64250f193a0c5057091cb044aa4a9be7
Sangin Han <hugedreamhd@gmail.com>
2023-12-27 19:58:13 +0900

    오류: focusedBorder만 있으면 안되고 enableBorder도 같이 설정해줘야 TextField 창 모양이 변경이 안된다

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -21,6 +21,14 @@ class MainScreen extends StatelessWidget {
                       color: Colors.black, // 외곽선 컬러 설정
                     ),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    //필드코너를 약간 각지게 한다
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.black, // 외곽선 컬러 설정
+                    ),
+                  ),
                   hintText: '입력하세요',
                 ),
               ),
