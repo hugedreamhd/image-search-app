36f529dfcd806beef838f3413d6b385f41a9de31
Sangin Han <hugedreamhd@gmail.com>
2023-12-27 17:19:39 +0900

    TextField decoration으로 설정 줄때 외곽선 컬러 안먹음

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -9,13 +9,23 @@ class MainScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    //필드코너를 약간 각지게 한다
+                    borderSide: BorderSide(
+                      color: Color(0xFF4B6B2),
+                    ), // 외곽선 컬러 설정
+                  ),
+                  labelText: '입력하세요'),
+            ),
             Expanded(
               child: GridView.builder(
                 itemCount: 10,
                 itemBuilder: (context, index) {
                   return Text('111');
                 },
-
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2),
               ),
