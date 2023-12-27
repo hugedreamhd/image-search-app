5f569c40bf4ca79add7fd5f2ba182aabb3d32da8
Sangin Han <hugedreamhd@gmail.com>
2023-12-27 20:30:11 +0900

    ClipRRect를 이용해서 Image 위젯을 코너에 둥글게 만들어 줌

--- lib/widget/image_item_widget.dart
+++ lib/widget/image_item_widget.dart
@@ -5,9 +5,12 @@ class ImageItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      'https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg',
-      fit: BoxFit.cover,
+    return ClipRRect(//자식위젯을 둥근 모서리로 자르는데 사용
+      borderRadius: BorderRadius.circular(20.0), // 코너의 각도 조절
+      child: Image.network(
+        'https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg',
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
