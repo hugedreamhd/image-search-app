c1117463f1ef3bb19f21697031351a2bc75e0266
Sangin Han <hugedreamhd@gmail.com>
2023-12-27 20:53:47 +0900

    model 클래스 image_item.dart 를 만들고 imageUrl과 tags를 required constructor 생성 후Data class section 으로 tojson, fromjson 만들기

--- lib/data/model/image_item.dart
+++ lib/data/model/image_item.dart
@@ -21,7 +21,7 @@ class ImageItem {
 
   @override
   String toString() {
-    return 'ImageItem{' + ' imageUrl: $imageUrl,' + ' tags: $tags,' + '}';
+    return 'ImageItem{ imageUrl: $imageUrl, tags: $tags,}';
   }
 
   ImageItem copyWith({
@@ -34,14 +34,14 @@ class ImageItem {
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return {
-      'imageUrl': this.imageUrl,
-      'tags': this.tags,
+      'imageUrl': imageUrl,
+      'tags': tags,
     };
   }
 
-  factory ImageItem.fromMap(Map<String, dynamic> map) {
+  factory ImageItem.fromJson(Map<String, dynamic> map) {
     return ImageItem(
       imageUrl: map['imageUrl'] as String,
       tags: map['tags'] as String,
